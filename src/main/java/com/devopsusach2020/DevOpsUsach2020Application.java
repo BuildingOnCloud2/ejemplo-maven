99bdb2e8b9625ae1c6b3817c6b205541b6728535
Jesus Alberto Ruiz <jaruizf@falabella.cl>
2022-01-10 21:58:00 -0300

    Add Sonarqube execution with Quality Gates

--- src/main/java/com/devopsusach2020/DevOpsUsach2020Application.java
+++ src/main/java/com/devopsusach2020/DevOpsUsach2020Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevOpsUsach2020Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DevOpsUsach2020Application.class, args);
 	}
-
 }
